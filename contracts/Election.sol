e0b49a7a97375da3a453741491bac5e000cfcf85
Ajay R Warrier <ajayrwarrier@gmail.com>
2018-12-30 18:10:02 +0530

    Added Votnig Functionality

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.0;
+pragma solidity ^0.5.0;
 
 contract Election {
     // Model a Candidate
@@ -8,9 +8,10 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Read/write candidates
     mapping(uint => Candidate) public candidates;
-
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -19,9 +20,22 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
-    function addCandidate (string memory name) private{
+    function addCandidate (string memory _name) private {
         candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
 }
